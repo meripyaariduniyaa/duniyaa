0ab22ebf5118394272876b8404751ef825711205
Chandan Singh <meri.pyaari.duniyaa@gmail.com>
2026-08-06 22:53:11 +0530

    update

--- components/Header.js
+++ components/Header.js
@@ -14,8 +14,8 @@ export default function Header() {
 
   return (
     <nav className="topbar" id="site-header">
-      <Link href="/" className="logo" style={{ textDecoration: 'none' }}>
-        Lovely<span>Crafts</span>
+      <Link href="/" className="logo-link">
+        <img src="/lovelycrafts-logo.png" alt="Lovely Crafts" className="site-logo" />
       </Link>
       <div className="nav-links">
         <Link href="/create" className="nav-link">Create</Link>
